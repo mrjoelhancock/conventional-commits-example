f7e77fa673fb836688869344bd2607611d80de07
Joel Hancock <joel@joelhancock.com>
2020-02-13 14:18:34 +1100

    feat: add editor config rules

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parser: "babel-eslint",
+}
